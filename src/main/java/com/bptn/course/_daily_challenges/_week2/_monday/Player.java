a79fc31cfbcaa1c456f8dc2f2ccb92cf15716467
Jess <idahosajessie@gmail.com>
2024-10-03 00:23:43 -0400

    Initialized player class with attributes, methods,

--- src/main/java/com/bptn/course/_daily_challenges/_week2/_monday/Player.java
+++ src/main/java/com/bptn/course/_daily_challenges/_week2/_monday/Player.java
@@ -1,10 +1,59 @@
 package com.bptn.course._daily_challenges._week2._monday;
 
+import java.util.Scanner;
+
 public class Player {
+	
+	//Declare instance variables- player names, player numbers
+	private String name; // Specifies this player’s name
+	private int playerNumber; // Specifies the number of the player’s token
+	
+	//Create constructor for Player object
+	public Player(String name, int playerNumber) {
+		this.name = name; // sets instance variable to the name argument
+		this.playerNumber = playerNumber;  // sets instance variable to the playerNumber argument
+	}
+	
+	//Create Scanner object
+	private static Scanner scan = new Scanner(System.in);
+	
+	//Create methods
+	
+	//Create getName() method- returns the player's name
+	public String getName() {
+		return name;
+	}
+	
+	//Create getPlayerNumber() method- returns the player's number
+		public String getPlayerNumber() {
+			return playerNumber;
+		}
+		
+	//Create makeMove() method- returns the value of the move made by player
+	public int makeMove() {
+		System.out.println("Make your move. What column do you want to put a token in?");
+		int column = scan.nextInt();
+		return column;
+	}
+	
+	//Create toString() method
+	@Override
+	public String toString() {
+		return "Player [" + playerNumber + "] is [" + name + "]";
+	}
+
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Player p1 = new Player("Uche", 1);
+		System.out.println(p1.name);
+		p1.toString();
 
 	}
 
 }
+
+//Player
+//create constructor that sets player's names and numbers to instance variables'
+//create getName() method and getPlayerNumber() method-- player number represents player token (1 = player1 etc.)
+//create makeMove method() -- prompt user for the column they want to drop their token in, then return the value
