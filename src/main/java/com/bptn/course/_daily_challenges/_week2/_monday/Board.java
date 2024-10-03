b083be24d79ae800eae5b36863cbaf975c1b6a4b
Jess <idahosajessie@gmail.com>
2024-10-03 00:04:00 -0400

    create package for Four-In-A-Row classes

---
+++ src/main/java/com/bptn/course/_daily_challenges/_week2/_monday/Board.java
@@ -0,0 +1,10 @@
+package com.bptn.course._daily_challenges._week2._monday;
+
+public class Board {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/bptn/course/_daily_challenges/_week2/_monday/Game.java
@@ -0,0 +1,24 @@
+package com.bptn.course._daily_challenges._week2._monday;
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+// Pseudocode
+//if board is full and no player has won, game ends in a draw
+//numPlayers must be min 2 or max 4.
+	// can initialize array to length of 4 then check that 2 are not null.
+// get player names, use for loop to check for duplicates
+// store names in an array, if new player == a name in array then prompt for a new name
+// prompt players to choose between 4-10 rows and 4-10 columns for game board
+
+//Player
+// create constructor that sets player's names and numbers to instance variables'
+// create getName() method and getPlayerNumber() method-- player number represents player token (1 = player1 etc.)
+// create makeMove method() -- prompt user for the column they want to drop their token in, then return the value
